46638d3361aef312b329822cd11918c5c3f8d310
Vitor Stelmastchuk <vssantos6@stefanini.com>
2022-10-13 11:42:29 -0300

    teste

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -16,6 +16,7 @@ describe('Signup Controller', () => {
     const httpResponse = await sut.handle(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
 
   })
 
