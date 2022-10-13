f7df313440fd1be79b5b848364227d59ac865c0a
Vitor Stelmastchuk <vssantos6@stefanini.com>
2022-10-13 11:46:23 -0300

    teste

--- src/presentation/controllers/signUpController.ts
+++ src/presentation/controllers/signUpController.ts
@@ -1,5 +1,6 @@
 export class SignUpController {
     async handle(httpRequest: any): Promise<any> {
-        return { statusCode: 400, body: new Error('Missing param: name') }
+        if (!httpRequest.body.name) return { statusCode: 400, body: new Error('Missing param: name') }
+        if (!httpRequest.body.email) return { statusCode: 400, body: new Error('Missing param: email') }
     }
 }
--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -21,4 +21,23 @@ describe('Signup Controller', () => {
   })
 
 
+  test('Should return 400 if no email is provided', async () => {
+
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+
+  })
+
+
 })
