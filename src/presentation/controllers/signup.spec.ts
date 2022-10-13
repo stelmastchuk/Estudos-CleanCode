c152248136bc6b1f8cf0f55117abbf48001b86b0
Vitor Stelmastchuk <vssantos6@stefanini.com>
2022-10-13 11:38:02 -0300

    add test

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,23 @@
+import { SignUpController } from './signUpController'
+
+describe('Signup Controller', () => {
+
+  test('Should return 400 if no name is provided', async () => {
+
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+
+  })
+
+
+})
