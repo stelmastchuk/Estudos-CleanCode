7ed953ed0a5c8481131ca54d274487fd65e8de34
Vitor Stelmastchuk <vssantos6@stefanini.com>
2022-10-13 11:42:19 -0300

    add SignupController

--- src/presentation/controllers/signUpController.ts
+++ src/presentation/controllers/signUpController.ts
@@ -1,5 +1,5 @@
 export class SignUpController {
     async handle(httpRequest: any): Promise<any> {
-        return { statusCode: 400 }
+        return { statusCode: 400, body: new Error('Missing param: name') }
     }
 }
